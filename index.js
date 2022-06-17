5ccce8455adc2470cb70e898c2324e74f78c90fa
ivakhelo_e <elizavetaivahnenko3@gmail.com>
2022-06-17 22:15:42 +0300

    dom

---
+++ index.js
@@ -0,0 +1,70 @@
+
+//2 задание
+// let ul = document.querySelectorAll('ul li');
+// function m(array) {
+// 	for (let i = 0; i < array.length; i++) {
+// 		if (i <= 3) {
+// 			array[i].classList.add('first');
+// 		}
+// 		return ul;
+// 	}
+// }
+// console.log(m(ul))
+
+//хотела при помощи функции первому и третьему элементу задать класс но почему то задалось только первому
+
+//и по нормальному))))
+
+let li = document.querySelectorAll('ul li');
+li[0].classList.add('first','bkg');
+li[1].classList.add('second','bkg');
+li[2].classList.add('third','bkg');
+li[3].classList.add('fourth','bkg');
+li[4].classList.add('fifth','bkg');
+li[5].classList.add('sixth','bkg');
+li[6].classList.add('seventh','bkg');
+
+// // li[0].innerHTML = 'red';  попробовала, как вариант но больше понравился след. вариант:
+let l1 = document.querySelector('.first').innerHTML = 'red';
+let l2 = document.querySelector('.second').innerHTML = 'orange';
+let l3 = document.querySelector('.third').innerHTML = 'yellow';
+let l4 = document.querySelector('.fourth').innerHTML = 'green';
+let l5 = document.querySelector('.fifth').innerHTML = 'forget color)';
+let l6 = document.querySelector('.sixth').innerHTML = 'blue';
+let l7 = document.querySelector('.seventh').innerHTML = 'purple';
+
+let arrayElementsLI = document.querySelectorAll('li.bkg');
+console.log(arrayElementsLI);
+arrayElementsLI.forEach(el => { el.style.backgroundColor = 'pink' });
+
+//1 task
+
+// let test=document.createElement('div');
+// let testP = document.createElement('p');
+// document.body.prepend(test);
+// test.append(testP);
+//пробовала создавать элементы через ДОМ потом решила их добавить в html  а то так очень долго)))
+
+let arr = document.querySelectorAll('div p');
+arr.forEach(el => { el.innerHTML = "paragraf" });
+let arrayOrange=document.querySelectorAll('p.bkg')
+for (let i = 0; i < arrayOrange.length; i++){
+	arrayOrange[i].style.backgroundColor = 'orange';
+}
+
+
+//task 3
+
+let product = document.querySelector('div.productContainer');
+let h1 = document.createElement('h1');
+let price = document.createElement('h3');
+product.append(h1);
+product.append(price);
+h1.innerHTML = 'Compucter';
+price.innerHTML = '25$';
+product.style.border = '1px solid black';
+product.style.width = '300px';
+/// не могу центрировать элементы по центру)) хотела задать align-item or flex but I can't find information about this ....
+
+
+
