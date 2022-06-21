8d88dd99b940139508fb14cc531be976b8584a22
ivakhelo_e <elizavetaivahnenko3@gmail.com>
2022-06-21 23:04:52 +0300

    static finish

--- index.2.js
+++ index.2.js
@@ -1,7 +1,76 @@
 
+class Worker{
+	static id = 1
+	static countWorker = 0
+	static sumSalary=[]
+	constructor(name, category, salary) {
+		this.id = Worker.id
+		this.name = name;
+		this.category = category;
+		this.salary = salary;
+		Worker.id++
+		Worker.countWorker++
+		Worker.sumSalary.push(this.salary)
+	}
+	showSalary() {
+		this._id++;
+		console.log(`employee with id ${this.id} have salary ${this.salary} USD per month`);
+		
+	}
+	countSalaryPerYear() {
+		this.salary *= 12;
+		return this.salary;
+	}
+	countAllWorkers() {
+		console.log( Worker.countWorker);
+	}
+	sumSalary() {
+		let salaryArray = Worker.sumSalary;
+		const sum =salaryArray.reduce(function (sum, elem) {
+			return (sum + elem)*12;
+		}, 0);
+		console.log(sum);
+		return sum;
+	}
+}
+let developer = new Worker('Ivan', 'developer', 3000);
+let manager = new Worker('Sam', 'manager', 2500);
+developer.showSalary();
+manager.showSalary();
+
+manager.countAllWorkers();
+manager.sumSalary();
 
 
+// task 2
 
+class PartTimeWorker extends Worker{
+	static countWorker2=0
+	constructor(name,category,salary,hours,bid) {
+		super(name, category, salary);
+		this.hours = hours;
+		this.bid = bid;
+		PartTimeWorker.countWorker2++
+	}
+	countbidPerDay() {
+		let bidPerDay = this.hours * this.bid;
+		console.log(bidPerDay);
+		return bidPerDay;
+	}
+	countSalaryPerMonth() {
+		const sumSalaryPerMonth = (this.hours*this.bid) * 22;
+		console.log(sumSalaryPerMonth);
+		Worker.sumSalary.push(this.sumSalaryPerMonth); //?
+		return sumSalaryPerMonth;
+	}
+	countPartTimeWorker() {
+		console.log(PartTimeWorker.countWorker2);
+	}
+}
 
-
+let t2 = new PartTimeWorker('Liza', 'ds', 3000, 8, 50);
+t2.countbidPerDay();
+t2.countSalaryPerMonth();
+t2.countPartTimeWorker();
+Worker.sumSalary
 
--- index.js
+++ index.js
@@ -15,27 +15,36 @@
 
 //и по нормальному))))
 
-let li = document.querySelectorAll('ul li');
-li[0].classList.add('first','bkg');
-li[1].classList.add('second','bkg');
-li[2].classList.add('third','bkg');
-li[3].classList.add('fourth','bkg');
-li[4].classList.add('fifth','bkg');
-li[5].classList.add('sixth','bkg');
-li[6].classList.add('seventh','bkg');
+// let li = document.querySelectorAll('ul li');
+// li[0].classList.add('first','bkg');
+// li[1].classList.add('second','bkg');
+// li[2].classList.add('third','bkg');
+// li[3].classList.add('fourth','bkg');
+// li[4].classList.add('fifth','bkg');
+// li[5].classList.add('sixth','bkg');
+// li[6].classList.add('seventh','bkg');
 
 // // li[0].innerHTML = 'red';  попробовала, как вариант но больше понравился след. вариант:
-let l1 = document.querySelector('.first').innerHTML = 'red';
-let l2 = document.querySelector('.second').innerHTML = 'orange';
-let l3 = document.querySelector('.third').innerHTML = 'yellow';
-let l4 = document.querySelector('.fourth').innerHTML = 'green';
-let l5 = document.querySelector('.fifth').innerHTML = 'forget color)';
-let l6 = document.querySelector('.sixth').innerHTML = 'blue';
-let l7 = document.querySelector('.seventh').innerHTML = 'purple';
+// let l1 = document.querySelector('.first').innerHTML = 'red';
+// let l2 = document.querySelector('.second').innerHTML = 'orange';
+// let l3 = document.querySelector('.third').innerHTML = 'yellow';
+// let l4 = document.querySelector('.fourth').innerHTML = 'green';
+// let l5 = document.querySelector('.fifth').innerHTML = 'forget color)';
+// let l6 = document.querySelector('.sixth').innerHTML = 'blue';
+// let l7 = document.querySelector('.seventh').innerHTML = 'purple';
 
-let arrayElementsLI = document.querySelectorAll('li.bkg');
-console.log(arrayElementsLI);
-arrayElementsLI.forEach(el => { el.style.backgroundColor = 'pink' });
+// let arrayElementsLI = document.querySelectorAll('li.bkg');
+// console.log(arrayElementsLI);
+// arrayElementsLI.forEach(el => { el.style.backgroundColor = 'pink' });
+
+let arr1 = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh'];
+let arr2 = ['red', 'orange', 'yellow', 'green', 'forget color', 'blue', 'purple'];
+let arrEl = [];
+let ul = document.querySelectorAll('ul li');
+for (let i = 0; i < 7; i++) {
+    arrEl[i] = (document.querySelectorAll(arr1[i]).innerHTML = arr2[i]);
+}
+
 
 //1 task
 
@@ -45,26 +54,47 @@ arrayElementsLI.forEach(el => { el.style.backgroundColor = 'pink' });
 // test.append(testP);
 //пробовала создавать элементы через ДОМ потом решила их добавить в html  а то так очень долго)))
 
-let arr = document.querySelectorAll('div p');
-arr.forEach(el => { el.innerHTML = "paragraf" });
-let arrayOrange=document.querySelectorAll('p.bkg')
-for (let i = 0; i < arrayOrange.length; i++){
-	arrayOrange[i].style.backgroundColor = 'orange';
-}
+// let arr = document.querySelectorAll('div p');
+// arr.forEach(el => { el.innerHTML = "paragraf" });
+// let arrayOrange=document.querySelectorAll('p.bkg')
+// for (let i = 0; i < arrayOrange.length; i++){
+// 	arrayOrange[i].style.backgroundColor = 'orange';
+// }
 
 
 //task 3
 
-let product = document.querySelector('div.productContainer');
-let h1 = document.createElement('h1');
-let price = document.createElement('h3');
-product.append(h1);
-product.append(price);
-h1.innerHTML = 'Compucter';
-price.innerHTML = '25$';
-product.style.border = '1px solid black';
-product.style.width = '300px';
+// let product = document.querySelector('div.productContainer');
+// let h1 = document.createElement('h1');
+// let price = document.createElement('h3');
+// product.append(h1);
+// product.append(price);
+// h1.innerHTML = 'Compucter';
+// price.innerHTML = '25$';
+// product.style.border = '1px solid black';
 /// не могу центрировать элементы по центру)) хотела задать align-item or flex but I can't find information about this ....
+
+// const userTest = {
+// 	login: 'Vasya',
+// 	password:'query',
+// }
+
+// let el;
+// let contElem=document.querySelector('#container');
+// for (let value in userTest) {
+// 	el = document.createElement('p');
+// 	el.innerHTML = value;
+// 	contElem.append(el);
+// }
+
+// arrText.forEach((el,i,a) => {contElem.append(makeElement('p'))
+	
+// })
+
+
+
+
+
 
 
 
